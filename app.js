76bd06c9d69d7457763338499bf5a669b1a9cb2a
AshiqurRahaman02 <ashiqur999999@gamil.com>
2024-10-17 16:15:44 +0530

    Updated shared-repo submodule to commit 7aa92f2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const sharedRepo = require('./shared-repo/index');
 
-const num = 10; 
+const num = 10;
 
 const result1 = sharedRepo.multiplyByFive(num);
 console.log(`10 multiplied by 5 is: ${result1}`);
